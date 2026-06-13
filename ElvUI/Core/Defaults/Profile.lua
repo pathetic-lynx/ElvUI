bb0b8837a76b02344f7c182f06b1266cdc5c325f
pathetic-lynx <no@mail.com>
2026-06-13 17:08:30 +0200

    feat: integrate swingbar

--- ElvUI/Core/Defaults/Profile.lua
+++ ElvUI/Core/Defaults/Profile.lua
@@ -1663,6 +1663,26 @@ P.unitframe = {
 				enable = true,
 				position = "TOPLEFT"
 			},
+			swingbar = {
+				enable = false,
+				width = 270,
+				height = 18,
+				spacing = 0,
+				color = {r = 0.31, g = 0.31, b = 0.31},
+				backdropColor = {r = 0.31, g = 0.31, b = 0.31},
+				spark = true,
+				verticalOrientation = false,
+				text = {
+					enable = true,
+					position = "CENTER",
+					xOffset = 0,
+					yOffset = 2,
+					font = "Homespun",
+					fontSize = 10,
+					fontOutline = "MONOCHROMEOUTLINE",
+					color = {r = 1, g = 1, b = 1}
+				}
+			},
 			CombatIcon = {
 				enable = true,
 				defaultColor = true,
